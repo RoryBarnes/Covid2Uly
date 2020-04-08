41b1dbdcd862be6545312e049bc11cd02a33d8a0
Rory Barnes <rory@Rorys-MBP-2.domain>
2020-04-08 11:26:27 -0700

    Added more columns, including rates per capita.

--- CovidTracking2Uly.py
+++ CovidTracking2Uly.py
@@ -51,122 +51,123 @@ def fnDays(sDate):
         sDate += 'Aug'
 
     sDate += ' 2020'
+    #print (sDate)
     return iNumDays,sDate
 
 def fnState(sAbbrev):
     # iState,sState,iPopulation
     if sAbbrev == "AK":
-        return 0,"Alaska"
+        return 0,"Alaska",0.732
     if sAbbrev == "AL":
-        return 1,"Alabama"
+        return 1,"Alabama",4.903
     if sAbbrev == "AR":
-        return 2,"Arkansas"
+        return 2,"Arkansas",3.018
     if sAbbrev == "AS":
-        return 3,"American Samoa"
+        return 3,"American Samoa",0.056
     if sAbbrev == "AZ":
-        return 4,"Arizona"
+        return 4,"Arizona",7.279
     if sAbbrev == "CA":
-        return 5,"California"
+        return 5,"California",39.512
     if sAbbrev == "CO":
-        return 6,"Colorado"
+        return 6,"Colorado",5.729
     if sAbbrev == "CT":
-        return 7,"Connecticut"
+        return 7,"Connecticut",3.565
     if sAbbrev == "DC":
-        return 8,"District of Columbia"
+        return 8,"District of Columbia",0.706
     if sAbbrev == "DE":
-        return 9,"Delaware"
+        return 9,"Delaware",0.974
     if sAbbrev == "FL":
-        return 10,"Florida"
+        return 10,"Florida",21.478
     if sAbbrev == "GA":
-        return 11,"Georgia"
+        return 11,"Georgia",10.617
     if sAbbrev == "GU":
-        return 12,"Guam"
+        return 12,"Guam",0.166
     if sAbbrev == "HI":
-        return 13,"Hawaii"
+        return 13,"Hawaii",1.416
     if sAbbrev == "IA":
-        return 14,"Iowa"
+        return 14,"Iowa",3.155
     if sAbbrev == "ID":
-        return 15,"Idaho"
+        return 15,"Idaho",1.787
     if sAbbrev == "IL":
-        return 16,"Illinois"
+        return 16,"Illinois",12.671
     if sAbbrev == "IN":
-        return 17,"Indiana"
+        return 17,"Indiana",6.732
     if sAbbrev == "KS":
-        return 18,"Kansas"
+        return 18,"Kansas",2.913
     if sAbbrev == "KY":
-        return 19,"Kentucky"
+        return 19,"Kentucky",4.468
     if sAbbrev == "LA":
-        return 20,"Louisiana"
+        return 20,"Louisiana",4.649
     if sAbbrev == "MA":
-        return 21,"Massachusetts"
+        return 21,"Massachusetts",6.950
     if sAbbrev == "MD":
-        return 22,"Maryland"
+        return 22,"Maryland",6.046
     if sAbbrev == "ME":
-        return 23,"Maine"
+        return 23,"Maine",1.344
     if sAbbrev == "MI":
-        return 24,"Michigan"
+        return 24,"Michigan",9.987
     if sAbbrev == "MN":
-        return 25,"Minnesota"
+        return 25,"Minnesota",5.640
     if sAbbrev == "MO":
-        return 26,"Missourri"
+        return 26,"Missourri",6.137
     if sAbbrev == "MP":
-        return 27,"Northern Mariana Islands"
+        return 27,"Northern Mariana Islands",0.055
     if sAbbrev == "MS":
-        return 28,"Mississippi"
+        return 28,"Mississippi",2.976
     if sAbbrev == "MT":
-        return 29,"Montana"
+        return 29,"Montana",1.069
     if sAbbrev == "NC":
-        return 30,"North Carolina"
+        return 30,"North Carolina",10.488
     if sAbbrev == "ND":
-        return 31,"North Dakota"
+        return 31,"North Dakota",0.762
     if sAbbrev == "NE":
-        return 32,"Nebraska"
+        return 32,"Nebraska",1.934
     if sAbbrev == "NH":
-        return 33,"New Hampshire"
+        return 33,"New Hampshire",1.360
     if sAbbrev == "NJ":
-        return 34,"New Jersey"
+        return 34,"New Jersey",8.882
     if sAbbrev == "NM":
-        return 35,"New Mexico"
+        return 35,"New Mexico",2.097
     if sAbbrev == "NV":
-        return 36,"Nevada"
+        return 36,"Nevada",3.080
     if sAbbrev == "NY":
-        return 37,"New York"
+        return 37,"New York",19.454
     if sAbbrev == "OH":
-        return 38,"Ohio"
+        return 38,"Ohio",11.689
     if sAbbrev == "OK":
-        return 39,"Oklahoma"
+        return 39,"Oklahoma",3.957
     if sAbbrev == "OR":
-        return 40,"Oregon"
+        return 40,"Oregon",4.218
     if sAbbrev == "PA":
-        return 41,"Pennsylvania"
+        return 41,"Pennsylvania",12.802
     if sAbbrev == "PR":
-        return 42,"Puerto Rico"
+        return 42,"Puerto Rico",3.194
     if sAbbrev == "RI":
-        return 43,"Rhode Island"
+        return 43,"Rhode Island",1.059
     if sAbbrev == "SC":
-        return 44,"South Carolina"
+        return 44,"South Carolina",5.149
     if sAbbrev == "SD":
-        return 45,"South Dakota"
+        return 45,"South Dakota",0.885
     if sAbbrev == "TN":
-        return 46,"Tennessee"
+        return 46,"Tennessee",6.833
     if sAbbrev == "TX":
-        return 47,"Texas"
+        return 47,"Texas",28.996
     if sAbbrev == "UT":
-        return 48,"Utah"
+        return 48,"Utah",3.206
     if sAbbrev == "VA":
-        return 49,"Virginia"
+        return 49,"Virginia",8.536
     if sAbbrev == "VI":
-        return 50,"Virgin Islands"
+        return 50,"Virgin Islands",0.105
     if sAbbrev == "VT":
-        return 51,"Vermont"
+        return 51,"Vermont",0.624
     if sAbbrev == "WA":
-        return 52,"Washington"
+        return 52,"Washington",7.615
     if sAbbrev == "WI":
-        return 53,"Wisconsin"
+        return 53,"Wisconsin",5.822
     if sAbbrev == "WV":
-        return 54,"West Virginia"
+        return 54,"West Virginia",1.792
     if sAbbrev == "WY":
-        return 55,"Wyoming"
+        return 55,"Wyoming",0.579
 
 iNumStates = 56
 #iNumDays = 27
@@ -245,8 +246,12 @@ iaHospNew = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
 iaNegNew = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
 iaPosNew = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
 iaTestsNew = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
-iaPercentPos = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
+daPercentPos = [[0.0 for i in range(iNumDays)] for j in range(iNumStates)]
+iaDeathsMillion = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
+iaPosMillion = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
+iaTestsMillion = [[0 for i in range(iNumDays)] for j in range(iNumStates)]
 saState = ["" for i in range(iNumStates)]
+iaPop = [0 for i in range(iNumStates)]
 saDate = ["" for i in range(iNumDays)]
 
 # Rest csv file and read in header
@@ -261,8 +266,9 @@ for saLine in csvData:
     #print(sDate+' '+repr(iDaysSince04Mar))
     saDate[iDaysSince04Mar-1] = sDateNew
     #print(repr(iDaysSince04Mar))
-    iState,sState = fnState(sAbbrev)
+    iState,sState,iPopulation = fnState(sAbbrev)
     saState[iState] = sState
+    iaPop[iState] = iPopulation
     #print(sState,repr(iState),repr(iDaysSince04Mar))
     if saLine[2] != '':
         iaPositive[iState][iDaysSince04Mar-1] = int(saLine[2])
@@ -300,20 +306,31 @@ for saLine in csvData:
         iaTestsNew[iState][iDaysSince04Mar-1] = int(saLine[24])
 
     if iaTestsNew[iState][iDaysSince04Mar-1] > 0:
-        iaPercentPos[iState][iDaysSince04Mar-1] = iaPosNew[iState][iDaysSince04Mar-1]/iaTestsNew[iState][iDaysSince04Mar-1]
+        #print("% Pos > 0")
+        daPercentPos[iState][iDaysSince04Mar-1] = float(iaPosNew[iState][iDaysSince04Mar-1])/iaTestsNew[iState][iDaysSince04Mar-1]
+        #print(repr(iaPosNew[iState][iDaysSince04Mar-1]), repr(iaTestsNew[iState][iDaysSince04Mar-1]), repr(daPercentPos[iState][iDaysSince04Mar-1]))
     else:
-        iaPercentPos[iState][iDaysSince04Mar-1] = 0
+        daPercentPos[iState][iDaysSince04Mar-1] = 0.0
+
+
+    #print(repr(daPercentPos[iState][iDaysSince04Mar-1]))
+    iaDeathsMillion[iState][iDaysSince04Mar-1] = iaDeaths[iState][iDaysSince04Mar-1]/iaPop[iState]
+    iaPosMillion[iState][iDaysSince04Mar-1] = iaPositive[iState][iDaysSince04Mar-1]/iaPop[iState]
+    iaTestsMillion[iState][iDaysSince04Mar-1] = iaTests[iState][iDaysSince04Mar-1]/iaPop[iState]
+
 
 sOut='covid19-US-'+saDate[-1].replace(" ", "") +'.csv'
 OutFile=open(sOut,'w')
 
-sOutLine=',State ID,Days Since Mar 3,'
+sOutLine=',State ID,Days Since Mar 4,'
 sOutLine += 'Cumulative Deaths,New Deaths,'
-sOutLine += 'Cumulative Positive,New Positive,\% Positive,'
+sOutLine += 'Cumulative Positive,New Positive,% Positive,'
 sOutLine += 'Cumulative Tests,New Tests,Recovered,'
 sOutLine += 'Cumulative Hospitalized,Current Hospitalized,New Hospitalized,'
 sOutLine += 'Cumulative in ICU,Currently in ICU,'
-sOutLine += 'Cumulative on Vent.,Currently on Vent.'
+sOutLine += 'Cumulative on Vent.,Currently on Vent.,'
+sOutLine += 'Deaths/Million,Pos./Million,Tests/Million,'
+sOutLine += 'Population,'
 sOutLine += '#State,#Date,'
 sOutLine += 'NULL\n'
 
@@ -324,12 +341,13 @@ for iState in range(iNumStates):
     for iDay in range(iNumDays):
         sOutLine = repr(iID)+','+repr(iState)+','+repr(iDay)+','
         sOutLine += repr(iaDeaths[iState][iDay])+','+repr(iaDeathsNew[iState][iDay])+','
-        sOutLine += repr(iaPositive[iState][iDay])+','+repr(iaPosNew[iState][iDay])+','+repr(iaPercentPos[iState][iDaysSince04Mar-1])+','
-        sOutLine += repr(iaTests[iState][iDay])+','+repr(iaTestsNew[iState][iDay])+','+repr(iaRecovered[iState][iDaysSince04Mar-1])+','
+        sOutLine += repr(iaPositive[iState][iDay])+','+repr(iaPosNew[iState][iDay])+','+repr(daPercentPos[iState][iDay])+','
+        sOutLine += repr(iaTests[iState][iDay])+','+repr(iaTestsNew[iState][iDay])+','+repr(iaRecovered[iState][iDay])+','
         sOutLine += repr(iaHospCum[iState][iDay])+','+repr(iaHospCur[iState][iDay])+','+repr(iaHospNew[iState][iDay])+','
         sOutLine += repr(iaICUCum[iState][iDay])+','+repr(iaICUCur[iState][iDay])+','
         sOutLine += repr(iaVentCum[iState][iDay])+','+repr(iaVentCur[iState][iDay])+','
-        #print(repr(iDay)+'  '+saDate[iDay])
+        sOutLine += repr(iaDeathsMillion[iState][iDay])+','+repr(iaPosMillion[iState][iDay])+','+repr(iaTestsMillion[iState][iDay])+','
+        sOutLine += repr(iaPop[iState])+','
         sOutLine += saState[iState]+','+saDate[iDay]+',-1\n'
         OutFile.write(sOutLine)
         iID += 1
